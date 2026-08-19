35d99a8a8f1edb4281c07131cfc125fb6bf5f5f3
battatt <badmaevbata@gmail.com>
2026-08-20 00:06:57 +0300

    WT Commit

---
+++ app/src/main/java/com/example/notes/domain/AddNoteUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.notes.domain
+
+class AddNoteUseCase(
+    private val repository: NotesRepository
+) {
+    operator fun invoke(note: Note) {
+        repository.addNote(note)
+    }
+}
