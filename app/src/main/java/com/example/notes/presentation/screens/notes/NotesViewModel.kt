980b104a5e964654a63efc9273851ff0b30a233d
battatt <badmaevbata@gmail.com>
2026-08-20 15:37:29 +0300

    NotesViewModel first version added

--- app/src/main/java/com/example/notes/presentation/screens/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/notes/presentation/screens/notes/NotesViewModel.kt
@@ -1,4 +1,99 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.example.notes.presentation.screens.notes
 
-class NotesViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.notes.data.TestNotesRepositoryImpl
+import com.example.notes.domain.AddNoteUseCase
+import com.example.notes.domain.DeleteNoteUseCase
+import com.example.notes.domain.EditNoteUseCase
+import com.example.notes.domain.GetAllNotesUseCase
+import com.example.notes.domain.GetNoteUseCase
+import com.example.notes.domain.Note
+import com.example.notes.domain.SearchNotesUseCase
+import com.example.notes.domain.SwitchPinnedStatusUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class NotesViewModel: ViewModel() {
+    private val repository = TestNotesRepositoryImpl  // Violation Clean Architecture
+
+    private val addNoteUseCase = AddNoteUseCase(repository)
+    private val editNoteUseCase = EditNoteUseCase(repository)
+    private val getAllNotesUseCase = GetAllNotesUseCase(repository)
+    private val deleteNoteUseCase = DeleteNoteUseCase(repository)
+    private val getNoteUseCase = GetNoteUseCase(repository)
+    private val searchNotesUseCase = SearchNotesUseCase(repository)
+    private val switchPinnedStatusUseCase = SwitchPinnedStatusUseCase(repository)
+
+    private val query = MutableStateFlow("")
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    private val _state = MutableStateFlow(NotesScreenState())
+    val state = _state.asStateFlow()
+
+    init {
+        query.flatMapLatest {
+            if (it.isBlank()) {
+                getAllNotesUseCase()
+            } else {
+                searchNotesUseCase(it)
+            }
+        }.onEach {
+            val pinnedNotes = it.filter { note -> note.isPinned }
+            val otherNotes = it.filter { note ->  !note.isPinned }
+            _state.update { screenState ->
+                screenState.copy(
+                    pinnedNotes = pinnedNotes,
+                    otherNotes = otherNotes,
+                )
+            }
+
+        }.launchIn(scope)
+    }
+
+    fun processCommand(command: NotesCommand) {
+        when (command) {
+            is NotesCommand.DeleteNote -> {
+                deleteNoteUseCase(command.noteId)
+            }
+            is NotesCommand.EditNote -> {
+                val title = command.note.title
+                editNoteUseCase(command.note.copy(title = "$title Edited!"))
+            }
+            is NotesCommand.InputSearchQuery -> {
+
+            }
+            is NotesCommand.SwitchPinnedStatus -> {
+
+            }
+        }
+    }
+
 }
+
+sealed interface NotesCommand {
+    data class InputSearchQuery(val query: String): NotesCommand
+    data class SwitchPinnedStatus(val noteId: Int): NotesCommand
+
+    // Temp
+
+    data class DeleteNote(val noteId: Int): NotesCommand
+    data class EditNote(val note: Note): NotesCommand
+}
+
+data class NotesScreenState(
+    val query: String = "",
+    val pinnedNotes: List<Note> = emptyList(),
+    val otherNotes: List<Note> = emptyList()
+)
